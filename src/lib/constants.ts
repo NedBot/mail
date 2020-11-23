ee413e97fa6f3afa8a561b79139838643f024c5e
George <t3ned.root@gmail.com>
2020-11-23 17:43:01 +0000

    feat(clientOptions): add client options ✨

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,43 @@
 import { KlasaClientOptions } from "klasa";
+import { clientCommandPrefix } from "../config";
+import { Permissions } from "../types/Enums";
 
-export const CLIENT_OPTIONS: KlasaClientOptions = {};
+export const CLIENT_OPTIONS: KlasaClientOptions = {
+	readyMessage: (client) => `Logged in as ${client.user!.tag}!`,
+	console: { utc: true, useColor: true, timestamps: "DD/MM/YY HH:mm:ss" },
+	customPromptDefaults: { quotedStringSupport: true },
+	consoleEvents: { verbose: true, debug: true },
+	providers: { default: "mongodb" },
+	schedule: { interval: 10000 },
+
+	prefix: clientCommandPrefix,
+	prefixCaseInsensitive: true,
+	slowmode: 600,
+	slowmodeAggressive: true,
+	commandMessageLifetime: 900,
+
+	messageCacheLifetime: 1200,
+	messageSweepInterval: 900,
+	messageCacheMaxSize: 50,
+
+	createPiecesFolders: false,
+	disabledCorePieces: ["commands"],
+
+	disableMentions: "everyone",
+	partials: ["MESSAGE", "REACTION"],
+	ws: {
+		intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_BANS", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS"]
+	},
+	pieceDefaults: {
+		commands: {
+			extendedHelp: "",
+			flagSupport: false,
+			permissionLevel: Permissions.Everyone,
+			quotedStringSupport: true,
+			runIn: ["text", "news"]
+		},
+		monitors: {
+			ignoreOthers: false
+		}
+	}
+};
