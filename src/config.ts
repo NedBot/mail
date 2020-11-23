356636aadc6f888052d19aba6df35208e47e5c6b
George <t3ned.root@gmail.com>
2020-11-23 20:45:24 +0000

    feat(config): add guild config ✨

--- src/config.ts
+++ src/config.ts
@@ -2,3 +2,6 @@ export const discordConnectionString = "Your discord token";
 export const mongodbConnectionString = "Your mongodb uri";
 export const mongodbDatabaseName = "NedMail";
 export const clientCommandPrefix = "?";
+
+export const mainGuildID = "685930872821645376";
+export const inboxGuildID = "";
